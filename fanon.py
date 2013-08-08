99b083816e467717f76f422e01a37f9eb3430c58
kwangde7 <kwangde7usa@gmail.com>
2013-08-08 11:31:50 -0700

    Update fanon.py
    getting rid of while loop

--- fanon.py
+++ fanon.py
@@ -2,11 +2,9 @@ import RPi.GPIO as GPIO
 import time
 
 GPIO.setmode(GPIO.BCM) 
-GPIO.setup(25, GPIO.OUT)
+GPIO.setup(23, GPIO.OUT)
 
-while True:
-	GPIO.output(25, GPIO.HIGH) 
-	time.sleep(1) # I want to turn the pin off after "20sec"
-	              # How to use the time function in python?
-	GPIO.output(25, GPIO.LOW) 
-	time.sleep(1)
+GPIO.output(23, GPIO.HIGH) 
+time.sleep(30) 
+GPIO.output(23, GPIO.LOW) 
+time.sleep(1)
