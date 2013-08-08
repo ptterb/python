d080e94b1badf57efe1e45717ba766cbce8ca856
kwangde7 <kwangde7usa@gmail.com>
2013-08-07 21:40:29 -0700

    comment added
    comment added

--- fanon.py
+++ fanon.py
@@ -6,6 +6,7 @@ GPIO.setup(25, GPIO.OUT)
 
 while True:
 	GPIO.output(25, GPIO.HIGH) 
-	time.sleep(1) 
+	time.sleep(1) # I want to turn the pin off after "20sec"
+	              # How to use the time function in python?
 	GPIO.output(25, GPIO.LOW) 
 	time.sleep(1)
