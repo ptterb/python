a98765a1d0742d75beace457ca02ec47fb4ef5b5
kwangde7 <kwangde7usa@gmail.com>
2013-08-16 14:00:24 -0700

    Create thankyouduck_twitter.py
    Recreate a thankyouduck file to thankyouduck_twitter.py 
    since github editing is not responding at the time of writing

---
+++ thankyouduck_twitter.py
@@ -0,0 +1,52 @@
+import subprocess
+from os.path import exists
+
+command = "twitter replies"  # the shell command
+process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=None, shell=True)
+
+#Launch the shell command:
+output = process.communicate()
+each_mentions = output[0].split("\n")
+first_mention = each_mentions[0]
+
+#Create a last mention file when this code first run
+if not exists('last_mention.txt'):
+    create_last_mention = open('last_mention.txt','w')
+    create_last_mention.write('This is last mention.txt')
+    create_last_mention.close()
+
+#open the last mention for comparison
+last_mention = open('last_mention.txt')
+last = last_mention.readline()
+
+#Read the most recent mention file and save in current.txt file
+current_mention = open('current.txt','w')
+current_mention.write(first_mention)
+current_mention.close()
+current_mention = open('current.txt')
+current = current_mention.readline()
+
+print "\n"
+print "Start compare current and last mention"
+
+#print "\n"
+#print "Read last mention :" + last
+#print "Read current mention:" + current
+if current == last:
+    print "Hmm...it seems like nobody mentions you at this time."
+
+elif not current == last:
+    print "Somebody mentioned you!! Let's inflate the ducky!"
+    if ":)" in current:
+        print "I am happy now:)"
+        subprocess.call(["python","on.py"])
+    elif ":(" in current:
+        print "I am sad now :("
+    else:
+        print " No command found"
+
+#store current to mention to last mention
+current_to_last = open('last_mention.txt','w')
+current_to_last.truncate()
+current_to_last.write(current)
+
