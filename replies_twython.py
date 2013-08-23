b02b7204f14e073e2d322c67f3f1fa6c2a263b60
kwangde7 <kwangde7usa@gmail.com>
2013-08-22 19:34:09 -0700

    Update and rename twython.py to replies_twython.py
    Updated to the final version. Modify folder path from
    ThankYouDuck to thankyouduck and rename it as replies_twitter.py

--- replies_twython.py
+++ replies_twython.py
@@ -1,3 +1,4 @@
+
 import sys
 from twython import Twython
 #Using oauth_1 to access personal account.
@@ -9,21 +10,23 @@ OAUTH_TOKEN= '1648701380-OpdNZv2hTQEmO1zgSa5tltnabU2waoDrCNAFlb0'
 OAUTH_TOKEN_SECRET='gr3iuLIVRbVIb0JBpIA2tuezfnN1oRiTLtn5pdNipg'
 
 #photo= open('/home/pi/ThankYouDuck/camera/my_gif.GIF')
-photo= open('/home/pi/ThankYouDuck/camera/ducky.jpg')
+photo= open('/home/pi/thankyouduck/camera/ducky.jpg')
 
 
 twitter = Twython(APP_KEY, APP_SECRET, OAUTH_TOKEN,OAUTH_TOKEN_SECRET)
 twitter.verify_credentials()
 
-who_mention = open('/home/pi/ThankYouDuck/who.txt')
+who_mention = open('/home/pi/thankyouduck/who.txt')
 
 user_data = who_mention.readline().split(" ")
 username = user_data[0]
 #tweet_id = user_data[len(user_data)-1]
 #status='@'+ username +'I am inflated :) '+ ''
-status= 'Thank you for feeding me. '+ '@'+ username+' ' + 'let\'s keep sharing $
-tweet_id =open('/home/pi/ThankYouDuck/tweet_id.txt').readline()
+status= 'Quack '+ '@'+ username+' '
+tweet_id =open('/home/pi/thankyouduck/tweet_id.txt').readline()
 
 twitter.update_status_with_media(media=photo,
                                  status=status,
                                  in_reply_to_status_id=tweet_id)
+print "Tweet back to the user"
+
